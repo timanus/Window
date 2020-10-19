0165a547e3639b726c16af22d858e8bcc001fa16
timanus <ulezko.2015@mail.ru>
2020-10-19 20:10:44 +0300

    second commit

---
+++ src/js/modules/forms.js
@@ -0,0 +1,65 @@
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
+	const form = document.querySelectorAll('form'),
+		  inputs = document.querySelectorAll('input'),
+		  windows = document.querySelectorAll('[data-modal]');
+
+	checkNumInputs('input[name="user_phone"]')
+	
+	const message = {
+		loading: 'Загрузка',
+		success: 'Спасибо! Скоро мы с вами свяжемся!',
+		failure: 'Что-то пошло не так...'
+	};
+
+	const postData = async (url, data) => {
+		document.querySelector('.status').textContent = message.loading;
+		let res = await fetch(url, {
+			method: 'POST',
+			body: data
+		});
+		return res.ok ? await res.text() : reject();
+	};
+
+	const clearInputs = () => {
+		inputs.forEach(item => {
+			item.value = '';
+		});
+	};
+
+	form.forEach(item => {
+		item.addEventListener('submit', e => {
+			event.preventDefault();
+
+			let statusMessage = document.createElement('div');
+			statusMessage.classList.add('status');
+			item.appendChild(statusMessage);
+
+			const formData = new FormData(item);
+			if (item.getAttribute('data-calc') === 'end') {
+				for (let key in state) {
+					formData.append(key, state[key]);
+				}
+			}
+
+			postData('assets/server.php', formData)
+				.then(res => {
+					console.log(res);
+					statusMessage.textContent = message.success;
+				})
+				.catch(() => statusMessage.textContent = message.failure)
+				.finally(() => {
+					clearInputs();
+					setTimeout(() => {
+						statusMessage.remove();
+						windows.forEach(item => item.style.display = 'none');
+						document.body.style.overflow = '';
+						for (let key in state) {delete state[key]}
+					}, 5000);
+				});
+		});
+	});
+};
+
+export default forms;
